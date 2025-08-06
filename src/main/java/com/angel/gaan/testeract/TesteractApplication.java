3533665e7683733fd5531ff480ccf5c16748285c
Angel Gallegos <agallegos@genkey.com>
2025-08-06 09:39:31 +0200

    First commit

---
+++ src/main/java/com/angel/gaan/testeract/TesteractApplication.java
@@ -0,0 +1,13 @@
+package com.angel.gaan.testeract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angel/gaan/testeract/TesteractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angel.gaan.testeract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
